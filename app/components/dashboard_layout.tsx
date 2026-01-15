c9bceefdf57128494643e04fb99096d14cc816e5
Chamara perera <wchamaraask@gmail.com>
2026-01-15 16:41:13 +0530

    Add dashboard components and update dependencies

---
+++ app/components/dashboard_layout.tsx
@@ -0,0 +1,22 @@
+import Navbar from './navbar';
+import Sidebar from './sidebar';
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1 p-8">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
