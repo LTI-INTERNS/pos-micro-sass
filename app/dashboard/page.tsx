ac7ae1418a398707812202bb6fe1abda43dff484
MDMAshen <malshaashen20@gmail.com>
2026-01-19 21:31:00 +0530

    feat: add all components to dashboard overview

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,24 @@
 import DashboardLayout from '../components/dashboard_layout';
+import DateRangeBar from '../components/dashboard/DateRangeBar';
+import StatCardGrid from '../components/dashboard/StatCardGrid';
+import ChartsSection from '../components/dashboard/ChartsSection';
+import StaffReportSection from '@/app/components/dashboard/StaffReportSection';
+import TopSellingSection from '@/app/components/dashboard/TopSellingSection';
+
 
 export default function DashboardPage() {
   return (
     <DashboardLayout>
-        <div className="text-gray-500 text-center mt-20">
-            <h1 className="text-2xl font-semibold">Dashboard Content</h1>
-        </div>
+      <div className="space-y-8">
+        <DateRangeBar />
+        <StatCardGrid />
+        <ChartsSection />
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-6">
+        <StaffReportSection />
+        <TopSellingSection />
+      </div>
+
     </DashboardLayout>
   );
 }
