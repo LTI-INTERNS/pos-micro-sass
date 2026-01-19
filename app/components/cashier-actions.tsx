bf61b66334ce675b9ff97c8a3b8358cad30dfedd
N.H.M.Dilhara <2022t01525@stu.cmb.ac.lk>
2026-01-19 17:18:09 +0530

    feat(cashier): add cashier action button components

---
+++ app/components/cashier-actions.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+type Props = {
+  onAdd?: () => void;
+  onEdit?: () => void;
+  onDelete?: () => void;
+  onDeactive?: () => void;
+  onExport?: () => void;
+};
+
+export default function CustomerActionsBar({
+  onAdd,
+  onEdit,
+  onDelete,
+  onDeactive,
+  onExport
+}: Props) {
+  return (
+    <div className="bg-white rounded-xl border border-gray-100 px-6 py-3">
+      <div className="grid grid-cols-3 gap-3">
+         <button
+          onClick={onDeactive}
+          className="w-full rounded-full border border-orange-400 bg-white py-2
+                     text-xs font-semibold text-orange-500
+                     hover:bg-orange-50 hover:shadow-sm
+                     transition"
+        >
+          Deactive Customer
+        </button>
+
+        
+        <button
+          onClick={onDelete}
+          className="w-full rounded-full border border-orange-400 bg-white py-2
+                     text-xs font-semibold text-orange-500
+                     hover:bg-orange-50 hover:shadow-sm
+                     transition"
+        >
+          Delete Customer
+        </button>
+
+        <button
+          onClick={onEdit}
+          className="w-full rounded-full border border-orange-400 bg-white py-2
+                     text-xs font-semibold text-orange-500
+                     hover:bg-orange-50 hover:shadow-sm
+                     transition"
+        >
+          Edit Customer
+        </button>
+
+        <button
+          onClick={onAdd}
+          className="w-full rounded-full bg-orange-500 py-2
+                     text-xs font-semibold text-white
+                     hover:bg-orange-600
+                     transition"
+        >
+          Add New Customer
+        </button>
+ <button
+          onClick={onExport}
+          className="w-full rounded-full border border-orange-400 bg-white py-2
+                     text-xs font-semibold text-orange-500
+                     hover:bg-orange-50 hover:shadow-sm
+                     transition"
+        >
+          Export CSV
+        </button>
+
+
+      </div>
+    </div>
+  );
+}
