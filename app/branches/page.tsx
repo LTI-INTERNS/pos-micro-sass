d73574386330318a122fef7724d66e501e5cd1c4
MadushanLSKK <Kmadushan396@gmail.com>
2026-01-19 20:15:57 +0530

    feat(branch): add branch action buttons

---
+++ app/branches/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashboardLayout from '../components/dashboard_layout'
+
+const Branchpage = () => {
+  return (
+    <DashboardLayout>
+      <div>Branch</div>
+    </DashboardLayout>
+  )
+}
+
+export default Branchpage
