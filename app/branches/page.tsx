a2649e5d3798b84633b9d4e35e6abc3952d6b424
MadushanLSKK <Kmadushan396@gmail.com>
2026-01-19 23:17:15 +0530

    feat(ui): add data range bar to branch and suppliermanagement page

--- app/branches/page.tsx
+++ app/branches/page.tsx
@@ -3,11 +3,13 @@ import DashboardLayout from '../components/dashboard_layout'
 import BranchStatCardGrid from '../components/branchStatCardGrid'
 import BranchActionBar from '../components/branch-actions'
 import BranchGrowthSection from '../components/branch-growth-section'
+import DateRangeBar from '../components/DateRangeBar'
 
 const Branchpage = () => {
   return (
     <DashboardLayout>
       <div className="w-full space-y-6"> 
+       <DateRangeBar/>
        <BranchStatCardGrid/>
        <BranchActionBar/>
        <BranchGrowthSection/>
--- app/suppliermanagement/page.tsx
+++ app/suppliermanagement/page.tsx
@@ -3,11 +3,13 @@ import SearchBar from "../components/supplier-searchbar";
 import CustomerActionsBar from "../components/supplier-actions";
 import CustomersTable from "../components/supplier-table";
 import StatCardGrid from "../components/supplierStatCardGrid";
+import DateRangeBar from "../components/DateRangeBar";
 
 export default function CustomersPage() {
   return (
     <DashboardLayout>
       <div className="w-full space-y-6">
+        <DateRangeBar />
         <StatCardGrid />
         <SearchBar />
         <CustomerActionsBar />
