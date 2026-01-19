7598e833bee721edf0c86c6d37efc8c7278b8fcb
Chamara perera <wchamaraask@gmail.com>
2026-01-19 11:48:49 +0530

    feat: add hamburger menu for mobile responsiveness

--- app/components/dashboard_layout.tsx
+++ app/components/dashboard_layout.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import Navbar from './navbar';
 import Sidebar from './sidebar';
 
@@ -6,14 +9,17 @@ interface DashboardLayoutProps {
 }
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      <Navbar />
-      <div className="flex flex-1 min-h-0">
-        <Sidebar />
-        <main className="flex-1 overflow-y-auto p-8">
-          {children}
-        </main>
+      
+      <Navbar toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+
+      <div className="flex flex-1 overflow-hidden"> 
+        <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+
+        <main className="flex-1 overflow-y-auto p-8">{children}</main>
       </div>
     </div>
   );
