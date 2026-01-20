b0b2b41ad38838c21cf3c201e79d27100ff66338
Chamara perera <wchamaraask@gmail.com>
2026-01-20 14:39:11 +0530

    feat: implement Profit calculation page with mockdataset

---
+++ app/ProfitCalculation/page.tsx
@@ -0,0 +1,64 @@
+import DashboardLayout from "../components/dashboard_layout";
+import DateRangeBar from "../components/DateRangeBar";
+import SearchBar from "../components/ProfitCalculation/SuppliersSearch";
+import ActionsBar from "../components/ProfitCalculation/ActionBar";
+import ProductTable, {Product} from "../components/ProfitCalculation/ProductsTable";
+import StatCardGrid from "../components/ProfitCalculation/ProfitStatCardGrid";
+
+const sampleProducts: Product[] = [
+  {
+    id: "001",
+    date: "2025.10.25",
+    category: "Inventory",
+    description: "Cleaning Supply",
+    profit: "4500.00",
+    payment: "Cash",
+  },
+  {
+    id: "002",
+    date: "2025.10.25",
+    category: "Inventory",
+    description: "Cleaning Supply",
+    profit: "4500.00",
+    payment: "Cash",
+  },
+  {
+    id: "003",
+    date: "2025.10.25",
+    category: "Inventory",
+    description: "Cleaning Supplies",
+    profit: "4500.00",
+    payment: "Cash",
+  },
+  {
+    id: "004",
+    date: "2025.11.25",
+    category: "Inventory",
+    description: "Cleaning Supply",
+    profit: "4500.00",
+    payment: "Cash",
+  },
+  {
+    id: "005",
+    date: "2025.12.25",
+    category: "Inventory",
+    description: "Cleaning Supply",
+    profit: "4500.00",
+    payment: "Cash",
+  },
+];
+
+
+export default function CustomersPage() {
+  return (
+    <DashboardLayout>
+      <div className="w-full space-y-6">
+        <DateRangeBar/>
+        <StatCardGrid />
+        <SearchBar />
+        <ActionsBar />
+        <ProductTable products={sampleProducts} />
+      </div>
+    </DashboardLayout>
+  );
+}
