cba7a3f0f7fe816813c34c2d7812435eee3ab999
Chamara perera <wchamaraask@gmail.com>
2026-01-20 15:28:44 +0530

    refactor(supplier search):

---
+++ app/Dashboard/page.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from '../components/dashboard_layout';
+
+export default function DashboardPage() {
+  return (
+    <DashboardLayout>
+        <div className="text-gray-500  text-center mt-20">
+            <h1 className="text-2xl font-semibold">Dashboard Content</h1>
+        </div>
+    </DashboardLayout>
+  );
+}
