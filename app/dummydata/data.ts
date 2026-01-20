3ead47a3930500533db2c00469d91506a36a0cf6
MadushanLSKK <Kmadushan396@gmail.com>
2026-01-20 11:34:56 +0530

    feat: add sample data for foodCard components

---
+++ app/dummydata/data.ts
@@ -0,0 +1,32 @@
+export const foods = [
+  {
+    id: 1,
+    name: "Steak sapi bakar",
+    price: 25.12,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ2R4RC6EEPKYvKcEJPa4N1Qq39XFHkXILHA&s",
+  },
+  {
+    id: 2,
+    name: "Ayam kentang",
+    price: 15.4,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqDxD6noPVm9TA2VV_jWve0joapdkuvDDY5wRFkG9GCxIeSnmF_1Vx-zk&s",
+  },
+  {
+    id: 3,
+    name: "Ikan santan",
+    price: 11.21,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3WcgGWLYXzyCf-gzIQI-Hf7ndsdgrcm_TtQ&s",
+  },
+  {
+    id: 4,
+    name: "Mie Kuah Pedas",
+    price: 10.4,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2XnzeXJcJ5tRbNVquaa889uHQzXz-kBh5ag&s",
+  },
+  {
+    id: 5,
+    name: "Kuah santan",
+    price: 5.50,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRALAGFcVj2zAWMAViVc4z4RQAFQFvO5sKSKw&s",
+  },
+];
