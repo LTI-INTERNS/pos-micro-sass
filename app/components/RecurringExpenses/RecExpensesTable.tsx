f042b470c6d8f5271ac097a977ea92f0d580fe4f
Chamara perera <wchamaraask@gmail.com>
2026-01-21 11:39:37 +0530

    feat: add no cashier found results and fix table name changes

--- app/components/RecurringExpenses/RecExpensesTable.tsx
+++ app/components/RecurringExpenses/RecExpensesTable.tsx
@@ -1,58 +1,67 @@
 "use client";
 
-export type Product = {
+export type RecurringExpenses = {
   id: string;
   date: string;
   category: string;
   description: string;
-  profit: string;
   payment: string;
   addedby: string;
 };
 
 type Props = {
-  products: Product[];
+  RecurringExpenses: RecurringExpenses[];
 };
 
-export default function ProductTable({ products }: Props) {
+export default function RecurringExpensesTable({ RecurringExpenses }: Props) {
   return (
-    <section className="bg-white rounded-xl border border-gray-100">
-      <div className="flex items-center justify-between px-6 py-3">
-        <h2 className="text-xs font-semibold text-gray-900">Products</h2>
+    <section className="bg-white rounded-xl border border-gray-100 p-4">
+      <div className="flex items-center justify-between px-2 py-3">
+        <h2 className="text-xs font-semibold text-gray-900">Recurring Expenses</h2>
       </div>
 
+      
       <div className="overflow-x-auto">
-        <table className="w-full text-xs">
-          <thead>
-            <tr className="border-b border-gray-100 text-gray-900">
-              <th className="px-6 py-2 text-left font-normal font-semibold" >ID</th>
-              <th className="px-6 py-2 text-left font-normal font-semibold">Date</th>
-              <th className="px-6 py-2 text-left font-normal font-semibold">Category</th>
-              <th className="px-6 py-2 text-left font-normal font-semibold">Description</th>
-              <th className="px-6 py-2 text-left font-normal font-semibold">Profit</th>
-              <th className="px-6 py-2 text-left font-normal font-semibold">Payment</th>
-              <th className="px-6 py-2 text-left font-normal font-semibold">Added By</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {products.map((c) => (
-              <tr
-                key={c.id}
-                className="border-b border-gray-100 hover:bg-gray-50 transition"
-              >
-                <td className="px-6 py-3 font-medium text-gray-900 ">{c.id}</td>
-                <td className="px-6 py-3 font-medium text-gray-900">{c.date}</td>
-                <td className="px-6 py-3 font-medium text-gray-900">{c.category}</td>
-                <td className="px-6 py-3 font-medium text-gray-900">{c.description}</td>
-                <td className="px-6 py-3 font-medium text-gray-900">{c.profit}</td>
-                <td className="px-6 py-3 font-medium text-gray-900">{c.payment}</td>
-                <td className="px-6 py-3 font-medium text-gray-900">{c.addedby}</td>
+        <div className="min-w-[100px]"> 
+          <table className="w-full text-xs table-auto">
+            <thead>
+              <tr className="border-b border-gray-100 text-gray-900">
+                <th className="px-6 py-2 text-left font-semibold">ID</th>
+                <th className="px-6 py-2 text-left font-semibold">Date</th>
+                <th className="px-6 py-2 text-left font-semibold">Category</th>
+                <th className="px-6 py-2 text-left font-semibold">Description</th>
+                <th className="px-6 py-2 text-left font-semibold">Payment</th>
+                <th className="px-6 py-2 text-left font-semibold">Added By</th>
               </tr>
-            ))}
+            </thead>
 
-          </tbody>
-        </table>
+            <tbody>
+              {RecurringExpenses.map((c) => (
+                <tr
+                  key={c.id}
+                  className="border-b border-gray-100 hover:bg-gray-50 transition"
+                >
+                  <td className="px-6 py-3 font-medium text-gray-900">{c.id}</td>
+                  <td className="px-6 py-3 font-medium text-gray-900">{c.date}</td>
+                  <td className="px-6 py-3 font-medium text-gray-900">{c.category}</td>
+                  <td className="px-6 py-3 font-medium text-gray-900">{c.description}</td>
+                  <td className="px-6 py-3 font-medium text-gray-900">{c.payment}</td>
+                  <td className="px-6 py-3 font-medium text-gray-900">{c.addedby}</td>
+                </tr>
+              ))}
+              {RecurringExpenses.length === 0 && (
+              <tr>
+                <td
+                  colSpan={7}
+                  className="px-6 py-6 text-center text-gray-400 text-xs"
+                >
+                  No cashiers found
+                </td>
+              </tr>
+            )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </section>
   );
