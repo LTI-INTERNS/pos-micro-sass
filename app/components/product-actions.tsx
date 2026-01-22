7ac46e8a46cf447fe4fcbb83e728e61704229117
N.H.M.Dilhara <2022t01525@stu.cmb.ac.lk>
2026-01-22 12:06:25 +0530

    Feat(reusable button) add reusable buttons components

--- app/components/product-actions.tsx
+++ app/components/product-actions.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import ActionButton from "@/app/components/Dashboard/common/ActionButton";
+
 type Props = {
   onAdd?: () => void;
   onEdit?: () => void;
@@ -13,57 +15,30 @@ export default function CashierActionsBar({
   onEdit,
   onDelete,
   onDeactive,
-  onExport,
 }: Props) {
   return (
     <div className="bg-white rounded-xl border border-gray-100 px-4 py-3">
       <div className="flex items-center gap-5">
-        
-        <button
+        <ActionButton
+          label="Add to Stock"
           onClick={onDeactive}
-          className="flex-1 rounded-full border border-orange-400 bg-white
-                     py-2 text-xs font-semibold text-orange-500
-                     hover:bg-orange-50 transition"
-        >
-          Deactive Cashier
-        </button>
+        />
 
-        <button
+        <ActionButton
+          label="Delete Product"
           onClick={onDelete}
-          className="flex-1 rounded-full border border-orange-400 bg-white
-                     py-2 text-xs font-semibold text-orange-500
-                     hover:bg-orange-50 transition"
-        >
-          Delete Cashier
-        </button>
+        />
 
-        <button
+        <ActionButton
+          label="Edit Product"
           onClick={onEdit}
-          className="flex-1 rounded-full border border-orange-400 bg-white
-                     py-2 text-xs font-semibold text-orange-500
-                     hover:bg-orange-50 transition"
-        >
-          Edit Cashier
-        </button>
+        />
 
-        <button
+        <ActionButton
+          label="Add New Product"
+          variant="primary"
           onClick={onAdd}
-          className="flex-1 rounded-full bg-orange-500
-                     py-2 text-xs font-semibold text-white
-                     hover:bg-orange-600 transition"
-        >
-          Add New Cashier
-        </button>
-
-        <button
-          onClick={onExport}
-          className="flex-1 rounded-full bg-orange-500
-                     py-2 text-xs font-semibold text-white
-                     hover:bg-orange-600 transition"
-        >
-          Export CSV
-        </button>
-
+        />
       </div>
     </div>
   );
