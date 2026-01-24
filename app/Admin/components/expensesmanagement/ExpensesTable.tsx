3257dca9c2cf1ff2425db301594c1b34874c3d7a
Chamara perera <wchamaraask@gmail.com>
2026-01-24 13:00:45 +0530

    feat: add ExpensesTable component to display a table of expenses

---
+++ app/Admin/components/expensesmanagement/ExpensesTable.tsx
@@ -0,0 +1,53 @@
+"use client";
+import CommonTable, { Column } from "@/app/Admin/components/common/CommonTable"; 
+
+export type Expenses = {
+  id: string;
+  date: string;
+  category: string;
+  description: string;
+  payment: string;
+  addedby: string;
+};
+
+type Props = {
+  Expenses: Expenses[];
+};
+
+export default function ExpensesTable({ Expenses }: Props) {
+  const columns: Column<Expenses>[] = [
+    {
+      key: "id",
+      label: "ID",
+    },
+    {
+      key: "date",
+      label: "Date",
+    },
+    {
+      key: "category",
+      label: "Category",
+    },
+    {
+      key: "description",
+      label: "Description",
+    },
+    {
+      key: "payment",
+      label: "Payment",
+    },
+    {
+      key: "addedby",
+      label: "Added By",
+    },
+  ];
+
+  return (
+    <CommonTable
+      title="Expenses"
+      data={Expenses} 
+      columns={columns}
+      emptyMessage="No Expenses found"
+    />
+  );
+}
