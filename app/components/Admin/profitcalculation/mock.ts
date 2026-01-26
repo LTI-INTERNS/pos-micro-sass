03195d919a84faa6d5a65b69338bda2ce9a650b9
Chamara perera <wchamaraask@gmail.com>
2026-01-26 12:39:01 +0530

    feat: create mock.ts for protit table

---
+++ app/components/Admin/profitcalculation/mock.ts
@@ -0,0 +1,44 @@
+import { Profit } from "@/app/components/Admin/profitcalculation/ProfitTable";
+
+export const mockProfits: Profit[] = [
+  {
+    id: "001",
+    date: "2025-10-25",
+    category: "Inventory",
+    description: "Cleaning Supply",
+    profit: "4500.00",
+    payment: "Cash",
+  },
+  {
+    id: "002",
+    date: "2025-10-25",
+    category: "Inventory",
+    description: "Cleaning Supply",
+    profit: "4500.00",
+    payment: "Cash",
+  },
+  {
+    id: "003",
+    date: "2025-10-25",
+    category: "Inventory",
+    description: "Cleaning Supplies",
+    profit: "4500.00",
+    payment: "Cash",
+  },
+  {
+    id: "004",
+    date: "2025-11-25",
+    category: "Inventory",
+    description: "Cleaning Supply",
+    profit: "4500.00",
+    payment: "Cash",
+  },
+  {
+    id: "005",
+    date: "2025-12-25",
+    category: "Inventory",
+    description: "Cleaning Supply",
+    profit: "4500.00",
+    payment: "Cash",
+  },
+];
