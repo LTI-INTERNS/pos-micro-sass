bb3b6fafe1f7b70554cc9bb47bbdc0aceed7f0dd
Chamara perera <wchamaraask@gmail.com>
2026-01-26 12:38:46 +0530

    feat: create mock.ts for expenses table

---
+++ app/components/Admin/expensesmanagement/mock.ts
@@ -0,0 +1,36 @@
+import { Expenses } from "@/app/components/Admin/expensesmanagement/ExpensesTable";
+
+export const mockExpenses: Expenses[] = [
+  {
+    id: "001",
+    date: "2025-10-25",
+    category: "Inventory",
+    description: "Cleaning Supply",
+    payment: "Cash",
+    addedby: "Admin",
+  },
+  {
+    id: "002",
+    date: "2025-10-25",
+    category: "Inventory",
+    description: "Cleaning Supply",
+    payment: "Cash",
+    addedby: "Admin",
+  },
+  {
+    id: "004",
+    date: "2025-11-25",
+    category: "Inventory",
+    description: "Cleaning Supply",
+    payment: "Cash",
+    addedby: "Admin",
+  },
+  {
+    id: "005",
+    date: "2025-12-25",
+    category: "Inventory",
+    description: "Cleaning Supply",
+    payment: "Cash",
+    addedby: "Admin",
+  },
+];
