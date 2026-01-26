82e2b91aa0b109b9ab8537978eb73307f93c4374
Chamara perera <wchamaraask@gmail.com>
2026-01-26 13:35:34 +0530

    "feat: Add Recurring Expenses Popup component"

---
+++ app/components/Admin/recexpenses/AddRecExpensesPopup.tsx
@@ -0,0 +1,76 @@
+"use client";
+import React from 'react'
+import ReusableForm , { FieldConfig } from '../common/ReusableForm'
+import PopupActions from '../common/PopupActions';
+import ModalShell from '../common/ModalShell';
+
+
+type AddRecExpensesPopupProps = {
+  open: boolean;
+  onClose: () => void;
+  onSave: (values: {
+    date: string;
+    category: string;
+    description: string;
+    amount: string;
+
+  }) => void;
+};
+const addRecExpenseFields: FieldConfig[] = [
+  { name: "date", label: "Date", placeholder: "Enter date", type: "text" },
+  { name: "category", label: "Category", placeholder: "Enter category", type: "text" },
+  { name: "description", label: "Description", placeholder: "Enter description", type: "text" },
+  { name: "amount", label: "Amount", placeholder: "Enter amount", type: "number" },
+];
+
+const AddRecExpensesPopup = ({ open, onClose, onSave }: AddRecExpensesPopupProps) => {
+   const [latestValues, setLatestValues] = React.useState<Record<string, string>>({});
+
+  return (
+  
+   <ModalShell
+      open={open}
+      title="Add Recurring Expense"
+      onClose={onClose}
+      widthClassName="w-[980px] max-w-[92vw]"
+    >
+
+
+   <div >
+    <ReusableForm
+          fields={addRecExpenseFields}
+          onSubmit={(values) => {
+            setLatestValues(values);
+            onSave(values as any);
+          }}
+        />
+
+    <div className="mt-10 flex items-center justify-center gap-x-6 ">
+          <a href="#" className="flex h-20 w-full items-center justify-center rounded-md border border-gray-200"> <img src="/Popcard.png"  className="h-10 w-auto object-contain" alt="card" /> </a>
+          <a href="#" className="flex h-20 w-full items-center justify-center rounded-md border border-gray-200"> <img src="/Popcash.png" className="h-10 w-auto object-contain" alt="cash" /> </a>
+    </div>
+
+      <div className="flex items-center justify-center">
+          <div className="w-105">
+            <PopupActions
+              actions={[
+                { label: "Cancel", onClick: onClose, variant: "secondary" },
+                {
+                  label: "Save",
+                  onClick: () => onSave(latestValues as any),
+                  variant: "primary",
+                },
+              ]}
+            />
+          </div>
+        </div>
+
+
+   </div>
+
+     
+   </ModalShell>  
+
+  )
+}
+export default AddRecExpensesPopup
