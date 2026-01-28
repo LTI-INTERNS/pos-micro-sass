90925c50787479c8075e095697698ae2b2185044
Chamara perera <wchamaraask@gmail.com>
2026-01-28 10:16:24 +0530

    "feat: Add basic Reports page"

---
+++ app/reports/page.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "../components/Admin/common/dashboard_layout";
+
+export default function DashboardPage() {
+  return (
+    <DashboardLayout>
+        <div className="text-gray-500 text-center mt-20">
+            <h1 className="text-2xl font-semibold">Reports Content</h1>
+        </div>
+    </DashboardLayout>
+  );
+}
