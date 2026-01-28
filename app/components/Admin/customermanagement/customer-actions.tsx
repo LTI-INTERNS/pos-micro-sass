22265c8696b98abea054ac31622aabfb9c589d94
Chamara perera <wchamaraask@gmail.com>
2026-01-28 10:11:16 +0530

    "feat: Update CustomerActionsBar - Add cursor-pointer "

--- app/components/Admin/customermanagement/customer-actions.tsx
+++ app/components/Admin/customermanagement/customer-actions.tsx
@@ -18,13 +18,12 @@ export default function CustomerActionsBar({
 
   return (
     <>
-      <div className="bg-white rounded-xl border border-gray-100 px-6 py-3">
         <div className="grid grid-cols-3 gap-3">
           <button
             onClick={onDelete}
             className="w-full rounded-full border border-orange-400 bg-white py-2
                        text-xs font-semibold text-orange-500
-                       hover:bg-orange-50 hover:shadow-sm
+                       hover:bg-orange-50 hover:shadow-sm cursor-pointer
                        transition"
           >
             Delete Customer
@@ -34,7 +33,7 @@ export default function CustomerActionsBar({
             onClick={onEdit}
             className="w-full rounded-full border border-orange-400 bg-white py-2
                        text-xs font-semibold text-orange-500
-                       hover:bg-orange-50 hover:shadow-sm
+                       hover:bg-orange-50 hover:shadow-sm cursor-pointer
                        transition"
           >
             Edit Customer
@@ -44,13 +43,12 @@ export default function CustomerActionsBar({
             onClick={() => setShowPopup(true)}
             className="w-full rounded-full bg-orange-500 py-2
                        text-xs font-semibold text-white
-                       hover:bg-orange-600
+                       hover:bg-orange-600 cursor-pointer
                        transition"
           >
             Add New Customer
           </button>
         </div>
-      </div>
 
       {showPopup && (
         <AddCustomerForm open={showPopup} 
