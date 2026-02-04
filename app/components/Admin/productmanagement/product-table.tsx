42922d76aaaec454c1b6972b2d7cd6d8557539b0
Chamara perera <wchamaraask@gmail.com>
2026-02-05 00:05:35 +0530

    feat: add product selection to product table

--- app/components/Admin/productmanagement/product-table.tsx
+++ app/components/Admin/productmanagement/product-table.tsx
@@ -1,6 +1,12 @@
 import CommonTable, { Column } from "@/app/components/Admin/common/CommonTable";
 import { Product } from "@/app/productmanagement/data";
 
+type Props = {
+  products: Product [];
+  selectedProduct: Product | null;
+  setSelectedProduct: (p: Product | null) => void;
+}
+
 const productColumns: Column<Product>[] = [
   { key: "id", label: "ID" },
   { key: "name", label: "Name" },
@@ -14,12 +20,21 @@ const productColumns: Column<Product>[] = [
 
 ];
 
-export default function ProductsTable({ products }: { products: Product[] }) {
+export default function ProductsTable({ products, selectedProduct, setSelectedProduct }: Props) {
   return (
     <CommonTable
       title="Products"
       data={products}
       columns={productColumns}
+      emptyMessage="No Product found"
+      selectedRowId={selectedProduct?.id}
+      onSelectRow={(row) => {
+        if (selectedProduct?.id === row.id) {
+          setSelectedProduct(null);
+        } else {
+          setSelectedProduct(row);
+        }
+      }} 
     />
   );
 }
