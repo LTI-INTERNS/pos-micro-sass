fb8643dd17af693dc74ea8428068d024eb1a60ea
Chamara perera <wchamaraask@gmail.com>
2026-02-14 13:25:04 +0530

    feat: update tutorial navigation component path

--- app/tutorials/page.tsx
+++ app/tutorials/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import CommonLayout from "@/app/components/saas/common/CommonLayout";
-import Navigation from "@/app/components/saas/companyCreation/Navigation";
+import Navigation from "@/app/components/saas/landing/Navigation";
 
 import GlassPolicyLayout from "@/app/components/saas/common/GlassPolicyLayout";
 import GlassAccordion from "@/app/components/saas/common/GlassAccordion";
@@ -10,9 +10,11 @@ import { tutorialsItems } from "@/app/components/saas/tutorials/mockData";
 export default function TutorialsPage() {
   return (
     <CommonLayout navbar={<Navigation />}>
+      <div className = "pt-24 pb-10">
       <GlassPolicyLayout title="Tutorials" backHref="/">
         <GlassAccordion items={tutorialsItems} defaultOpenId="getting-started" />
       </GlassPolicyLayout>
+      </div>
     </CommonLayout>
   );
 }
