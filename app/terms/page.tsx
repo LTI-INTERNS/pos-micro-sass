86fee9802f796761b03be76e4f8b7453a59f9250
Chamara perera <wchamaraask@gmail.com>
2026-02-14 13:24:54 +0530

    refactor: update terms page navigation import

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import CommonLayout from "@/app/components/saas/common/CommonLayout";
-import Navigation from "@/app/components/saas/companyCreation/Navigation";
+import Navigation from "@/app/components/saas/landing/Navigation";
 
 import GlassPolicyLayout from "@/app/components/saas/common/GlassPolicyLayout";
 import GlassAccordion from "@/app/components/saas/common/GlassAccordion";
@@ -10,9 +10,11 @@ import { termsItems } from "@/app/components/saas/termsofservices/mockData";
 export default function TermsPage() {
   return (
     <CommonLayout navbar={<Navigation />}>
+      <div className = "pt-24 pb-10">
       <GlassPolicyLayout title="Terms of Service" backHref="/">
         <GlassAccordion items={termsItems} defaultOpenId="intro" />
       </GlassPolicyLayout>
+      </div>
     </CommonLayout>
   );
 }
