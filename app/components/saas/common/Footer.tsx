fbe62d429a71a46dbed68e6232852bc425a40d6d
Chamara perera <wchamaraask@gmail.com>
2026-02-17 16:01:55 +0530

    fix: standardize footer link URLs to lowercase

--- app/components/saas/common/Footer.tsx
+++ app/components/saas/common/Footer.tsx
@@ -59,7 +59,7 @@ export default function Footer() {
                   </Link>
                 </li>
                 <li className="hover:text-white cursor-pointer">
-                  <Link href="/Pricing" className="hover:text-white transition">
+                  <Link href="/pricing" className="hover:text-white transition">
                     Pricing
                   </Link>
                   </li>
@@ -76,7 +76,7 @@ export default function Footer() {
                     </Link>
                   </li>
                 <li className="hover:text-white cursor-pointer">
-                  <Link href="/contactUs" className="hover:text-white transition">
+                  <Link href="/contactus" className="hover:text-white transition">
                     Contact Us
                   </Link></li>
               </ul>
